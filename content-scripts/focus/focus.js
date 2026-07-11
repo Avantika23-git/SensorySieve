71df1bb478cd45e98baf6212847f721e4c78b8e7
Avantika <Avantikabizy@gmail.com>
2026-07-11 14:13:43 +0530

    set up

---
+++ content-scripts/focus/focus.js
@@ -0,0 +1,16 @@
+(function(){
+    'use strict';
+    var clusterSelector = [
+        '.ads',
+        '[id*="banner"]',
+        'aside',
+        '.sidebar',
+        '[class*="promo"]'
+    ];
+
+    var clutterClass = 'ss-focus-clutter';
+    var frozenClass = 'ss-focus-frozen';
+    var gifFrozenClass = 'ss-focus-gif-frozen';
+    var gifSnapshotClass = 'ss-focus-gif-snapshot';
+    var globalFilterClass = 'ss-focus-global-filter';
+})
