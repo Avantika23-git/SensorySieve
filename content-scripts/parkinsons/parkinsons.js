9c6c63d3e9c060e2b9e3257a0c12bcbbe0758207
Miliya Eldhose <miliyaeldhose@gmail.com>
2026-07-11 13:44:49 +0530

    added simple css for parkinson mode

--- content-scripts/parkinsons/parkinsons.js
+++ content-scripts/parkinsons/parkinsons.js
@@ -13,13 +13,14 @@ let smoothX=0;
 let smoothY=0;
 const alpha=0.2;
 
+
 function updateSmoothCoordinates() {
     smoothX += (rawX - smoothX) * alpha;
   smoothY += (rawY - smoothY) * alpha;
     requestAnimationFrame(updateSmoothPosition);
 }
 
-updateSmoothCoordinates();
+
 
 const cursorHideStyle = document.createElement("style");
 cursorHideStyle.id = "ss-parkinsons-hide-cursor";
