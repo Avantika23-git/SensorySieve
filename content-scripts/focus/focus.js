7391a1722a1d97202179cb922eb0c04798c43562
Avantika <Avantikabizy@gmail.com>
2026-07-11 15:15:24 +0530

    clutter sieve

--- content-scripts/focus/focus.js
+++ content-scripts/focus/focus.js
@@ -13,4 +13,25 @@
     var gifFrozenClass = 'ss-focus-gif-frozen';
     var gifSnapshotClass = 'ss-focus-gif-snapshot';
     var globalFilterClass = 'ss-focus-global-filter';
+
+    var focusModeIsOn = false;
+    var pageWatcher = null;
+
+    function blurClutter() {
+    var selectorString = clutterSelectors.join(',');
+    var clutterElements = document.querySelectorAll(selectorString);
+ 
+    for (var i = 0; i < clutterElements.length; i++) {
+      clutterElements[i].classList.add(clutterClass);
+        }
+    }
+
+    function unblurClutter() {
+    var blurredElements = document.querySelectorAll('.' + clutterClass);
+ 
+    for (var i = 0; i < blurredElements.length; i++) {
+      blurredElements[i].classList.remove(clutterClass);
+        }
+  }
+
 })
